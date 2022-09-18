7dc3dd12521e70198d342573af47f590d21a645f
anwjrrp33 <anwjrrp33@gmail.com>
2022-09-18 22:08:04 +0900

    make Auth, Logout

--- models/User.js
+++ models/User.js
@@ -71,6 +71,19 @@ userSchema.methods.generateToken = function(cb) {
     })
 }
 
+userSchema.statics.findByToken = function (token, cb) {
+    let user = this
+    // 토큰 decode한다.
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        // 유저 아이디 이용하여 유저 찾은 후 
+        // 클라이언트에서 가져온 토큰과 데이터베이스에 보관된 토큰이 일치하는지 확인한다.
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return cb(err)
+            cb(null, user)
+        })
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
